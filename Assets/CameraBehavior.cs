fe33efab5bafccd88de86a7388452537c464a788
Arlo <adcruz94@gmail.com>
2015-12-22 02:01:15 +1300

    added mouse influenced camera and tilt(kind of)

--- Assets/CameraBehavior.cs
+++ Assets/CameraBehavior.cs
@@ -3,50 +3,46 @@ using System.Collections;
 
 
 
-public class CameraBehavior : MonoBehaviour {
+public class CameraBehavior : MonoBehaviour
+{
     public GameObject PlayerPos;
     public float FracLag = 0.2F;
+    public float screenShiftModifier = 0.5F;
     private Vector3 CameraPos;
     private Vector3 Player;
+    
 
 
-    //We do the actual interpolation in FixedUpdate(), since we're dealing with a rigidbody
+
     void Update()
     {
-        CameraPos = new Vector3(gameObject.GetComponent<Transform>().position.x, 10, gameObject.GetComponent<Transform>().position.z);
         Player = new Vector3(PlayerPos.GetComponent<Transform>().position.x, 10, PlayerPos.GetComponent<Transform>().position.z);
-            //to start another lerp)
-            gameObject.GetComponent<Transform>().position = Vector3.Lerp(CameraPos, Player, FracLag);
+        CameraPos = new Vector3(gameObject.GetComponent<Transform>().position.x, 10, gameObject.GetComponent<Transform>().position.z);
+        if (Input.GetMouseButton(0))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.x -= Screen.width / 2;
+            mousePos.y -= Screen.height / 2;
+            float mouseAngle = Mathf.Atan2(mousePos.y, mousePos.x);
+            float targetX = Mathf.Cos(mouseAngle) /  screenShiftModifier + Player.x;
+            float targetY = Mathf.Sin(mouseAngle) /  screenShiftModifier + Player.z;
+            gameObject.GetComponent<Transform>().position = Vector3.Lerp(CameraPos, new Vector3(targetX, 10, targetY), FracLag);
+        }
+        else {
+            if (Mathf.Abs(CameraPos.x - Player.x) > 0.1 || Mathf.Abs(CameraPos.z - Player.z) > 0.1)
+            {
+                gameObject.GetComponent<Transform>().position = Vector3.Lerp(CameraPos, Player, FracLag);
+            }
+        }
+        if (Input.GetKey("a"))
+        {
+            Quaternion turnAngle = Quaternion.Euler(70, -90, -90);
+            //Quaternion turnAngle = Quaternion.LookRotation(PlayerPos.GetComponent<Transform>().position - gameObject.GetComponent<Transform>().position + new Vector3(-5, 0, 0));
+            gameObject.GetComponent<Transform>().rotation = Quaternion.Lerp(gameObject.GetComponent<Transform>().rotation, turnAngle, FracLag);
+            
+        }
 
     }
-    /*
-    public float FracLag = 0.2F;
-    private float xPos;
-    private float zPos;
-    private float yPos = 10;
-    private float currentXPos;
-    private float currentYPos;
-    private float currentZPos;
-    
-    // Use this for initialization
-    void Start () {
-	    new Vector3 test = new Vector3.Lerp()
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        
-        xPos = PlayerPos.GetComponent<Transform>().position.x;
-        zPos = PlayerPos.GetComponent<Transform>().position.z;
-        currentXPos = gameObject.GetComponent<Transform>().position.x;
-        currentZPos = gameObject.GetComponent<Transform>().position.z;
-        //Debug.Log("xPos: " + xPos.ToString() + " zPos: " + zPos.ToString());
-        
 
-
-        }
-        gameObject.GetComponent<Transform>().position = Vector3.Lerp(gameObject.GetComponent<Transform>().position,  PlayerPos.GetComponent<Transform>().position, FracLag);
-       
-    }*/
 
 }
